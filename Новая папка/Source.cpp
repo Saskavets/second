fa292d9025548da28ab76c58d04c94de6b0fe363
Smilingg <33789514+Smilingg@users.noreply.github.com>
2019-07-14 22:23:49 +0300

    Add files via upload

---
+++ Новая папка/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "Добрый вечер! Марина!! :)" << endl;
+	cout << "Видишь мой код приветствия? если да, напиши в ВКонаткте. :)" << endl;
+	return 0;
+}
